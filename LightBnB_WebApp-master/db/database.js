fcff5928261587d77c4fce4999cb25afe2639793
Mehwish Iqbal <mehwishned21@gmail.com>
2023-09-09 07:49:51 -0500

    Write function addProperty

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -26,6 +26,49 @@ const getAllProperties = (options, limit = 10) => {
     .catch((err) => {
       console.log(err.message);
     });
+  // 1
+  // const queryParams = [];
+  // // 2
+  // let queryString = `
+  // SELECT properties.*, avg(property_reviews.rating) as average_rating
+  // FROM properties
+  // JOIN property_reviews ON properties.id = property_id
+  // `;
+
+  // // 3
+  // if (options.city) {
+  //   queryParams.push(`%${options.city}%`);
+  //   queryString += `WHERE city LIKE $${queryParams.length} `;
+  // }
+  // if(options.owner_id) {
+  //   queryParams.push(`${options.owner_id}`);
+  //   queryString += `WHERE owner_id LIKE $${queryParams.length}`;
+  //   queryString += ` AND`
+  // }
+  // if(options.minimum_price_per_night && options.maximum_price_per_night) {
+  //   queryParams.push(`${options.minimum_price_per_night}`*100);
+  //   queryParams.push(`${options.maximum_price_per_night}`*100);
+  //   queryString += `WHERE cost_per_night BETWEEN $${queryParams.length-1} and $${queryParams.length}`;
+  //   queryString += ` AND`;
+  // }
+  // if(options.minimum_rating) {
+  //   queryParams.push(`${options.minimum_rating}`);
+  //   queryString += `HAVING avg(property_reviews.rating) >= ${queryParams.length}`;
+  // }
+
+  // // 4
+  // queryParams.push(limit);
+  // queryString += `
+  // GROUP BY properties.id
+  // ORDER BY cost_per_night
+  // LIMIT $${queryParams.length};
+  // `;
+
+  // // 5
+  // console.log(queryString, queryParams);
+
+  // // 6
+  // return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
 // pool.query(`SELECT title FROM properties LIMIT 10;`)
@@ -38,15 +81,7 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  // let resolvedUser = null;
-  // for (const userId in users) {
-  //   const user = users[userId];
-  //   if (user && user.email.toLowerCase() === email.toLowerCase()) {
-  //     resolvedUser = user;
-  //   }
-  // }
-  // return Promise.resolve(resolvedUser);
-
+  
   return pool
   .query(
     `SELECT *
@@ -112,7 +147,6 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
- // return getAllProperties(null, 2);
 
   return pool
   .query(
@@ -156,11 +190,51 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+
+  return pool
+  .query(
+    `INSERT INTO properties (owner_id, 
+      title, 
+      description, 
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms,
+      country,
+      street,
+      city,
+      province,
+      post_code)
+    VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14) RETURNING *;`,
+    [ property.owner_id, 
+      property.title, 
+      property.description, 
+      property.thumbnail_photo_url,
+      property.cover_photo_url,
+      property.cost_per_night,
+      property.parking_spaces,
+      property.number_of_bathrooms,
+      property.number_of_bedrooms,
+      property.country,
+      property.street,
+      property.city,
+      property.province,
+      property.post_code])
+  .then((result) => {
+    console.log(result.rows[0])
+    return result.rows[0]
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
+
 
 module.exports = {
   getUserWithEmail,
